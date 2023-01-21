bfbbdd4869be298a1b6ed40650320d4d76c699a5
Artem Safin <artemsafin.webdev@gmail.com>
2023-01-21 19:22:27 +0300

    По конфигу

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,13 @@
 import postcssPresetEnv from "postcss-preset-env";
+import cssnano from "cssnano";
 
 export default {
   plugins: [
     postcssPresetEnv({
       stage: 1,
+    }),
+    cssnano({
+      preset: "default",
     }),
   ],
 };
