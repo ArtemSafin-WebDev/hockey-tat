11452aa3e51246c55776bc44340206416aec0fad
Artem Safin <artemsafin.webdev@gmail.com>
2023-02-01 23:05:50 +0300

    По бегущей строке

--- src/js/ticker.ts
+++ src/js/ticker.ts
@@ -1,3 +1,8 @@
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
 function ticker(
   selector: string = ".js-ticker",
   childSelector = ".js-ticker-inner"
@@ -8,14 +13,33 @@ function ticker(
 
   elements.forEach((element) => {
     const innerTicker = element.querySelector<HTMLDivElement>(childSelector);
+    if (!innerTicker) return;
+    let clones = [];
 
-    if (innerTicker) {
+    for (let i = 0; i < 3; i++) {
       const clone = innerTicker.cloneNode(true);
       const parent = innerTicker.parentElement;
       if (parent) {
         parent.append(clone);
+        clones.push(clone);
       }
     }
+
+    [...clones, innerTicker].forEach((item) => {
+      const tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: element,
+          start: "top bottom",
+          end: "bottom top",
+          scrub: 1,
+        },
+      });
+
+      tl.to(item, {
+        xPercent: -100,
+        duration: 1,
+      });
+    });
   });
 }
 
