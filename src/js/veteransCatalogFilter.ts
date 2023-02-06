7039922c22582a5ff393ef8cb45ef31b6799466a
Artem Safin <artemsafin.webdev@gmail.com>
2023-02-06 15:52:57 +0300

    По фильтру ветеранов

--- src/js/veteransCatalogFilter.ts
+++ src/js/veteransCatalogFilter.ts
@@ -5,10 +5,38 @@ export default function veteransCatalogFilter() {
     ".veterans-catalog__mobile-filters-btn"
   );
   const closeBtn = document.querySelector<HTMLButtonElement>(
-    ".veterans-catalog__filters-modal-close-btn"
+    ".veterans-catalog__filters-modal-submit-btn"
   );
   const modal = document.querySelector(".veterans-catalog__filters-modal");
   let open = false;
+
+  const resetBtn = document.querySelector<HTMLButtonElement>(
+    ".veterans-catalog__filters-modal-reset-btn"
+  );
+
+  const inputs = Array.from(
+    document.querySelectorAll<HTMLInputElement>(
+      ".veterans-catalog__filters-checkbox-input"
+    )
+  );
+
+  const mobileFiltersBtnText = document.querySelector<HTMLButtonElement>(
+    ".veterans-catalog__mobile-filters-btn-text"
+  );
+
+  const clearSearchBtn = document.querySelector(
+    ".veterans-catalog__search-close-btn"
+  );
+
+  const setText = () => {
+    const checkedInput = inputs.find((input) => input.checked);
+    if (checkedInput) {
+      const textElement = checkedInput.parentElement?.querySelector(
+        ".veterans-catalog__filters-checkbox-text"
+      );
+      mobileFiltersBtnText.textContent = textElement.textContent;
+    }
+  };
 
   const openModal = () => {
     if (open) return;
@@ -24,15 +52,48 @@ export default function veteransCatalogFilter() {
     open = false;
   };
 
-  if (openBtn && closeBtn && modal) {
-    console.log("Hello there");
-    openBtn.addEventListener("click", (event) => {
-      event.preventDefault();
-      openModal();
+  if (modal) {
+    if (openModal) {
+      openBtn.addEventListener("click", (event) => {
+        event.preventDefault();
+        openModal();
+      });
+    }
+
+    if (closeModal) {
+      closeBtn.addEventListener("click", (event) => {
+        event.preventDefault();
+        closeModal();
+      });
+    }
+  }
+
+  inputs.forEach((input) => {
+    input.addEventListener("change", () => {
+      setText();
     });
-    closeBtn.addEventListener("click", (event) => {
+  });
+
+  if (resetBtn) {
+    resetBtn.addEventListener("click", (event) => {
       event.preventDefault();
+      if (inputs.length) {
+        inputs[0].checked = true;
+      }
+
+      setText();
+
       closeModal();
+    });
+  }
+
+  if (clearSearchBtn) {
+    clearSearchBtn.addEventListener("click", (event) => {
+      event.preventDefault();
+      const input = clearSearchBtn.previousElementSibling as HTMLInputElement;
+      if (input) {
+        input.value = "";
+      }
     });
   }
 }
