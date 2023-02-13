e472646087c9100c6e9a0719fdb28a901decceae
Artem Safin <artemsafin.webdev@gmail.com>
2023-02-14 02:26:56 +0300

    Каталог воспитанников

---
+++ pages-data/vospit.js
@@ -0,0 +1,83 @@
+const vospit = {
+  "/vospit-catalog.html": {
+    title: "Воспитанники",
+    partners: [
+      {
+        src: "/partners/tatneft.svg",
+        title: "Генеральный спонсор",
+      },
+      {
+        src: "/partners/tatneft-azs.svg",
+        title: "Официальный спонсор",
+      },
+      {
+        src: "/partners/taneco.svg",
+        title: "Официальный спонсор",
+      },
+    ],
+    vospit: [
+      {
+        src: "/vospit/1.webp",
+        title: "Яруллин<br> Альберт",
+        role: "Защитник",
+      },
+      {
+        src: "/vospit/2.webp",
+        title: "Петров<br> Кирилл",
+        role: "Нападающий",
+      },
+      {
+        src: "/vospit/3.webp",
+        title: "Токранов<br> Василий",
+        role: "Защитник",
+      },
+      {
+        src: "/vospit/4.webp",
+        title: "Мусин<br> Дамир",
+        role: "Защитник",
+      },
+      {
+        src: "/vospit/5.webp",
+        title: "Якупов<br> Наиль",
+        role: "Нападающий",
+      },
+      {
+        src: "/vospit/6.webp",
+        title: "Абросимов<br> Роман",
+        role: "Защитник",
+      },
+      {
+        src: "/vospit/7.webp",
+        title: "Билялов<br> Тимур",
+        role: "Вратарь",
+      },
+      {
+        src: "/vospit/8.webp",
+        title: "Фазылзянов<br> Камиль",
+        role: "Вратарь",
+      },
+      {
+        src: "/vospit/9.webp",
+        title: "Хохряков<br> Пётр",
+        role: "Нападающий",
+      },
+      {
+        src: "/vospit/10.webp",
+        title: "Лукоянов<br> Артем",
+        role: "Нападающий",
+      },
+      {
+        src: "/vospit/11.webp",
+        title: "Обухов<br> Дмитрий",
+        role: "Нападающий",
+      },
+      {
+        src: "/vospit/12.webp",
+        title: "Хафизуллин<br> Динар",
+        role: "Защитник",
+      },
+    ],
+  },
+};
+
+export default vospit;
--- pages.config.js
+++ pages.config.js
@@ -3,6 +3,7 @@ import newsSingle from "./pages-data/newsSingle";
 import news from "./pages-data/news";
 import veteransCatalog from "./pages-data/veterans";
 import veteransDetail from "./pages-data/veteranDetail";
+import vospit from "./pages-data/vospit";
 
 const pagesConfig = {
   ...home,
@@ -10,6 +11,7 @@ const pagesConfig = {
   ...news,
   ...veteransCatalog,
   ...veteransDetail,
+  ...vospit,
 };
 
 export default pagesConfig;
