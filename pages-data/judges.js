a10ae87a1cfe694a3ad1cf02fbaa12575d27c180
Artem Safin <artemsafin.webdev@gmail.com>
2023-02-18 12:27:48 +0300

    Каталог судей

---
+++ pages-data/judges.js
@@ -0,0 +1,107 @@
+const judges = {
+  "/judges.html": {
+    title: "Судьи",
+    partners: [
+      {
+        src: "/partners/tatneft.svg",
+        title: "Генеральный спонсор",
+      },
+      {
+        src: "/partners/tatneft-azs.svg",
+        title: "Официальный спонсор",
+      },
+      {
+        src: "/partners/taneco.svg",
+        title: "Официальный спонсор",
+      },
+    ],
+    judges: [
+      {
+        src: "/judge.webp",
+        firstName: "Александр Николаевич",
+        lastName: "Андреев",
+        date: "03.06.1997 г.",
+        category: "3-я категория",
+      },
+      {
+        src: "/judge.webp",
+        firstName: "Александр Николаевич",
+        lastName: "Андреев",
+        date: "03.06.1997 г.",
+        category: "3-я категория",
+      },
+      {
+        src: "/judge.webp",
+        firstName: "Александр Николаевич",
+        lastName: "Андреев",
+        date: "03.06.1997 г.",
+        category: "3-я категория",
+      },
+      {
+        src: "/judge.webp",
+        firstName: "Александр Николаевич",
+        lastName: "Андреев",
+        date: "03.06.1997 г.",
+        category: "3-я категория",
+      },
+      {
+        src: "/judge.webp",
+        firstName: "Александр Николаевич",
+        lastName: "Андреев",
+        date: "03.06.1997 г.",
+        category: "3-я категория",
+      },
+      {
+        src: "/judge.webp",
+        firstName: "Александр Николаевич",
+        lastName: "Андреев",
+        date: "03.06.1997 г.",
+        category: "3-я категория",
+      },
+      {
+        src: "/judge.webp",
+        firstName: "Александр Николаевич",
+        lastName: "Андреев",
+        date: "03.06.1997 г.",
+        category: "3-я категория",
+      },
+      {
+        src: "/judge.webp",
+        firstName: "Александр Николаевич",
+        lastName: "Андреев",
+        date: "03.06.1997 г.",
+        category: "3-я категория",
+      },
+      {
+        src: "/judge.webp",
+        firstName: "Александр Николаевич",
+        lastName: "Андреев",
+        date: "03.06.1997 г.",
+        category: "3-я категория",
+      },
+      {
+        src: "/judge.webp",
+        firstName: "Александр Николаевич",
+        lastName: "Андреев",
+        date: "03.06.1997 г.",
+        category: "3-я категория",
+      },
+      {
+        src: "/judge.webp",
+        firstName: "Александр Николаевич",
+        lastName: "Андреев",
+        date: "03.06.1997 г.",
+        category: "3-я категория",
+      },
+      {
+        src: "/judge.webp",
+        firstName: "Александр Николаевич",
+        lastName: "Андреев",
+        date: "03.06.1997 г.",
+        category: "3-я категория",
+      },
+    ],
+  },
+};
+
+export default judges;
--- pages.config.js
+++ pages.config.js
@@ -5,6 +5,7 @@ import veteransCatalog from "./pages-data/veterans";
 import veteransDetail from "./pages-data/veteranDetail";
 import vospit from "./pages-data/vospit";
 import vospitDetail from "./pages-data/vospitDetail";
+import judges from "./pages-data/judges";
 
 const pagesConfig = {
   ...home,
@@ -14,6 +15,7 @@ const pagesConfig = {
   ...veteransDetail,
   ...vospit,
   ...vospitDetail,
+  ...judges,
 };
 
 export default pagesConfig;
